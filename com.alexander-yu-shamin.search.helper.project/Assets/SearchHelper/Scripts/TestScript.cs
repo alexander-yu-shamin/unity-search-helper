03299285f9ac4c0afab5dfda2d7cdb3b30c2ba4d
alexander-yu-shamin <alexander.yu.shamin@gmail.com>
2025-09-07 23:11:42 +0300

    Add testing objects

---
+++ com.alexander-yu-shamin.search.helper.project/Assets/SearchHelper/Scripts/TestScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SearchHelper.Scripts
+{
+    public class TestScript : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer DefaultSpriteRenderer;
+        [SerializeField] private SpriteRenderer ReferenceSpriteRenderer;
+        [SerializeField] private TestScriptableObjectScript referenceScriptableObjectScript;
+    }
+}
---
+++ com.alexander-yu-shamin.search.helper.project/Assets/SearchHelper/Scripts/TestScriptableObjectScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace SearchHelper.Scripts
+{
+    public class TestScriptableObjectScript : ScriptableObject
+    {
+        [SerializeField] private Texture2D Icon;
+        [SerializeField] private string AssetGuid;
+    }
+}
